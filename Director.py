883d1683479434d858fcd07ee163461fc0368f1f
Randy <randygoitia7@gmail.com>
2022-12-11 12:10:47 -0400

    agregando nuevos agentes

---
+++ Director.py
@@ -0,0 +1,11 @@
+class Director():
+
+    def reglas_director(self):
+        reglas = {
+            2: 'Solicitar',
+            3: 'Dictar',
+            4: 'Elegir',
+            5: 'Entregar',
+            6: 'Confirmar',
+            7: 'Devolver'}
+        return reglas
---
+++ Encargado.py
@@ -0,0 +1,7 @@
+class Encargado():
+
+    def reglas_encargado(self):
+        reglas = {1: 'Entregar',
+                  2: 'Habilitar',
+                  3: 'Confirmar'}
+        return reglas
